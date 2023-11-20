1ef4cca8f1820639178b574a982705d52da9086f
gyleboi <act.gmirasol@gmail.com>
2023-11-20 18:45:27 +0800

    1.1

---
+++ SitInMonitoring/SitInMonitoring/Controllers/LogINController1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SitInMonitoring.Controllers
+{
+    public class LogINController1 : Controller
+    {
+        public IActionResult LoginForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+
+        }
+    }
+}
---
+++ SitInMonitoring/SitInMonitoring/Program.cs
@@ -0,0 +1,36 @@
+namespace SitInMonitoring
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=LogINController1}/{action=LoginForm}/{id?}");
+
+            app.Run();
+        }
+    }
+}
